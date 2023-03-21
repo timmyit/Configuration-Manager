55971f8242226888c847f951932d25865c92b017
Timmy Andersson <46671111+timmyit@users.noreply.github.com>
2023-03-21 16:43:44 +0100

    Add files via upload

---
+++ Export-RunScripts.ps1
@@ -0,0 +1,50 @@
+﻿#.NOTES
+#===========================================================================
+#Created on:    3/21/2023
+#Modified on:   3/21/2023
+#Created by:    Timmy Andersson
+#Twitter:       @TimmyITdotcom
+#Blog:          www.timmyit.com
+#===========================================================================
+#.DESCRIPTION
+#Export Run scripts. 
+#Specify Destination path with the parameter $DestinationPath
+ 
+[CmdletBinding(DefaultParameterSetName = 'DestinationPath')]
+param
+(
+[Parameter(Mandatory = $true,
+Position = 1)]
+$DestinationPath
+)
+    Begin{
+    $SiteCodeObjs = Get-WmiObject -Namespace "root\SMS" -Class SMS_ProviderLocation -ComputerName $env:COMPUTERNAME -ErrorAction Stop
+        foreach ($SiteCodeObj in $SiteCodeObjs)
+        {
+            if ($SiteCodeObj.ProviderForLocalSite -eq $true)
+                {
+                $SiteCode = $SiteCodeObj.SiteCode
+                }
+        }
+    $SitePath = $SiteCode + ":"
+    Import-module ($Env:SMS_ADMIN_UI_PATH.Substring(0, $Env:SMS_ADMIN_UI_PATH.Length - 5) + '\ConfigurationManager.psd1')
+}
+PROCESS
+{
+ 
+Set-Location $SitePath
+$Scripts = Get-CMScript 
+
+    if (-not (Test-Path $DestinationPath))
+    {
+        new-item -Path $DestinationPath -ItemType Directory -Force
+    }
+
+foreach ($Script in $Scripts) {
+
+  $PS = [System.Text.Encoding]::unicode.GetString([System.Convert]::FromBase64String($($Script).Script))
+   Out-File -FilePath "$DestinationPath\$($Script.ScriptName).ps1" -InputObject $PS
+ }
+
+ }
+
